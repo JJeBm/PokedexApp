02f9fa7bfd65fa2efca0fb66a1effae8f7fdbeca
jose.benitez <jose.benitez@konecta.com.py>
2024-11-12 22:57:34 -0300

    DEV - Estilo

--- src/screens/PokemonDetailsScreen.tsx
+++ src/screens/PokemonDetailsScreen.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import { View, Text, Image, useWindowDimensions, StyleSheet, FlatList } from 'react-native';
 import { PokemonTypeImage } from '../util/ui';
+import { typeColors } from '../util/ui/typesComponent';
 
 const PokemonDetailsScreen = ({ route }: any) => {
   const { pokemon } = route.params;
   const { height } = useWindowDimensions();
   const isSingleType = pokemon.types.length === 1;
-  console.log(pokemon.moves)
+  const primary = typeColors[pokemon.types[0]?.type.name]
+  const secundary = typeColors[pokemon.types[1]?.type.name]
+  const primary_2 = primary + "50"
+  const secundary_2 = secundary + "50"
+
+  const getColor = (type = 1) => {
+    if (type === 2 && secundary) {
+      return [secundary, secundary_2]
+    }
+    return [primary, primary_2]
+  }
   return (
     <View style={[styles.container, { height }]}>
       <Text style={styles.title}>{pokemon.name.toUpperCase()}</Text>
@@ -19,11 +30,13 @@ const PokemonDetailsScreen = ({ route }: any) => {
         ))}
       </View>
 
-      <Text style={styles.text}>Peso: {pokemon.weight / 10} kg</Text>
-      <Text style={styles.text}>Altura: {pokemon.height / 10} m</Text>
+      <View style={{ backgroundColor: getColor(1)[1], borderRadius: 20, padding: 10, borderColor: getColor(1)[0], borderWidth: 1 }}>
+        <Text style={styles.text}>Peso: {pokemon.weight / 10} kg</Text>
+        <Text style={styles.text}>Altura: {pokemon.height / 10} m</Text>
+      </View>
 
       <Text style={styles.subTitle}>Habilidades</Text>
-      <View>
+      <View style={{ backgroundColor: getColor(2)[1], borderRadius: 20, padding: 10, borderColor: getColor(2)[0], borderWidth: 1 }}>
         {pokemon.abilities.map((ability: { ability: { name: string }, is_hidden: boolean }) => (
           <Text key={ability.ability.name} style={styles.text}>
             {ability.ability.name} {ability.is_hidden ? "(Hidden)" : ""}
@@ -32,30 +45,33 @@ const PokemonDetailsScreen = ({ route }: any) => {
       </View>
 
       <Text style={styles.subTitle}>Estadísticas</Text>
-      {pokemon.stats.map((stat: { base_stat: number; stat: { name: string } }) => (
-        <View style={styles.statsContainer}>
-          <Text style={styles.text}>
-            {stat.stat.name.toUpperCase()}
-          </Text>
-          <Text style={styles.text}>
-            {stat.base_stat}
-          </Text>
-        </View>
-      ))}
+      <View style={{ backgroundColor: getColor(1)[1], borderRadius: 20, padding: 10, borderColor: getColor(1)[0], borderWidth: 1 }}>
+        {pokemon.stats.map((stat: { base_stat: number; stat: { name: string } }) => (
+          <View style={styles.statsContainer}>
+            <Text style={styles.text}>
+              {stat.stat.name.toUpperCase()}
+            </Text>
+            <Text style={styles.text}>
+              {stat.base_stat}
+            </Text>
+          </View>
+        ))}
+      </View>
 
       <Text style={styles.subTitle}>Movimientos</Text>
-      <FlatList
-        data={pokemon.moves}
-        style={styles.movesContainer}
-        numColumns={2}
-        columnWrapperStyle={styles.columnWrapperStyle}
-        keyExtractor={(item) => item.move.name}
-        renderItem={({ item }) => (
-          <Text style={styles.text}>
-            {`${item.move.name}`}
-          </Text>
-        )}
-      />
+      <View style={{ backgroundColor: getColor(2)[1], borderRadius: 20, padding: 10, borderColor: getColor(2)[0], borderWidth: 1, flex:1 }}>
+        <FlatList
+          data={pokemon.moves}
+          numColumns={2}
+          columnWrapperStyle={styles.columnWrapperStyle}
+          keyExtractor={(item) => item.move.name}
+          renderItem={({ item }) => (
+            <Text style={styles.text}>
+              {`${item.move.name}`}
+            </Text>
+          )}
+        />
+      </View>
     </View>
   );
 };
@@ -77,14 +93,14 @@ const styles = StyleSheet.create({
     width: 150,
     height: 150,
     alignSelf: 'center',
-    marginBottom: 10,
+    marginBottom: 0,
   },
   text: {
-    fontSize: 16,
+    fontSize: 13,
     marginVertical: 2,
   },
   subTitle: {
-    fontSize: 20,
+    fontSize: 15,
     fontWeight: '600',
     marginTop: 15,
     marginBottom: 5,
